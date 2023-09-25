66881407df2bc9747818cf4f3daf6913d0da9b13
Ana Clara <anaclaranoronha.m@gmail.com>
2023-09-25 09:30:21 -0300

    Fix: axios connections

--- src/axios/axios.connection.ts
+++
@@ -1,5 +0,0 @@
-import axios, { AxiosInstance, CreateAxiosDefaults } from 'axios';
-
-export const axios_github : AxiosInstance = create_axios_instance({
-    baseURL: "https://avatars.githubusercontent.com/u/108290807?s=400&u=21f41df43c0cf30265366fafa0aab7e9bbed731a&v=4",
-})
--- src/services/profile.service.ts
+++
@@ -1,7 +0,0 @@
-
-export class ProfileService{
-
-    public async set_photo(){
-        return await axios_github
-    }
-}
